c9c767f7cf4d5f852bcaa4729cfca803a653670c
devmoi <moishadrack.com>
2022-12-25 12:37:57 +0300

    add projects files

---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import '../App.css'
+
+const Footer = () => {
+  return (
+    <div className='footer'>
+        <p>Shopping 2022 &copy;</p>
+    </div>
+  )
+}
+
+export default Footer
---
+++ src/components/Navbar.js
@@ -0,0 +1,12 @@
+ import React from 'react'
+ import '../App.css'
+ 
+ const Navbar = () => {
+   return (
+     <div  className='navbar'>
+        <h1>Shopping App</h1>
+     </div>
+   )
+ }
+ 
+ export default Navbar
