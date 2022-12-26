a55cf4989f0d2ed5cc02bcb00ab372cf436895c6
devmoi <moishadrack.com>
2022-12-26 17:27:38 +0300

    add changes

--- src/App.js
+++ src/App.js
@@ -3,18 +3,18 @@ import './App.css';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import Products from './components/Products';
-import Login from './components/Login';
-import { useState } from 'react';
+// import Login from './components/Login';
+// import { useState } from 'react';
 
 function App() {
-  const [token,setToken]=useState(localStorage.getItem('userToken') ?? null)
+  // const [token,setToken]=useState(localStorage.getItem('userToken') ?? null)
 
   return (
     <div className="App">
       
       <Navbar />
-      {token ? <Products />: <Login token={token} setToken={setToken}/>}
-      
+      {/* {token ? : <Login token={token} setToken={setToken}/>} */}
+      <Products />
       <Footer />
     </div>
   );
--- src/components/Login.js
+++ src/components/Login.js
@@ -2,12 +2,16 @@ import React, {useState} from 'react'
 import '../App.css'
 import axios from 'axios';
 
+
 const Login = () => {
   const {userName, setUsername}=useState("");
   const {password, setPassword}=useState("");
-  const {error, stError}=useState("");
+  const {error, setError}=useState("");
   
-  const loginHandler= ({token,setToken})=>{
+  const LoginHandler= ({token,setToken})=>{
+    // setError("");
+    // setPassword("");
+    // setUsername("");
    axios ({
     url:'https://fakestoreapi.com/auth/login',
     method:"POST",
@@ -19,7 +23,8 @@ const Login = () => {
     console.log(res.data.token);
     setToken(res.data.token)
    }).catch(err=>{
-    console.log(err)
+    console.log(err.response.data);
+    setError(err.response)
    })
   }
 
@@ -30,9 +35,12 @@ const Login = () => {
            onChange={(e)=>setUsername(e.target.value)}
             type="text" 
             placeholder='Username'/>
-          <input value={password} onChange={(e)=>setPassword(e.target.value)} type="password" placeholder='Password'/>
-          <small>This is error placeholder</small>
-          <button onClick={loginHandler}>Login</button>
+          <input value={password} 
+             onChange={(e)=>setPassword(e.target.value)}
+           type="password" 
+           placeholder='Password'/>
+          {error && <small>{Error}</small>}
+          <button onClick={LoginHandler}>Login</button>
         </div>
     </div>
   )
