b3f75ccbeca82095b03a65f26488d974fd0e6292
devmoi <moishadrack.com>
2022-12-26 10:28:50 +0300

    add changes

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,20 +1,38 @@
 import React, {useState} from 'react'
 import '../App.css'
+import axios from 'axios';
 
 const Login = () => {
   const {userName, setUsername}=useState("");
   const {password, setPassword}=useState("");
   const {error, stError}=useState("");
+  
+  const loginHandler= ({token,setToken})=>{
+   axios ({
+    url:'https://fakestoreapi.com/auth/login',
+    method:"POST",
+    data:{
+      username: userName,
+      password: password,
+    },
+   }).then(res=>{
+    console.log(res.data.token);
+    setToken(res.data.token)
+   }).catch(err=>{
+    console.log(err)
+   })
+  }
+
   return (
-    <div className='login-inputs'>
-        <div>
+    <div className='login'>
+        <div className='login-inputs'>
           <input value={userName} 
            onChange={(e)=>setUsername(e.target.value)}
             type="text" 
             placeholder='Username'/>
-          <input value={password} onChange={(e)=>setPassword(e.target.value)} type="password" aria-placeholder='Password'/>
+          <input value={password} onChange={(e)=>setPassword(e.target.value)} type="password" placeholder='Password'/>
           <small>This is error placeholder</small>
-          <button>Login</button>
+          <button onClick={loginHandler}>Login</button>
         </div>
     </div>
   )
