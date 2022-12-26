808105ea78ce855c75bcede9a8fe998aefb66945
devmoi <moishadrack.com>
2022-12-26 09:31:09 +0300

    style the login

--- src/components/Login.js
+++ src/components/Login.js
@@ -6,9 +6,12 @@ const Login = () => {
   const {password, setPassword}=useState("");
   const {error, stError}=useState("");
   return (
-    <div className='login'>
+    <div className='login-inputs'>
         <div>
-          <input value={userName} onChange={(e)=>setUsername(e.target.value)} type="text" placeholder='Username'/>
+          <input value={userName} 
+           onChange={(e)=>setUsername(e.target.value)}
+            type="text" 
+            placeholder='Username'/>
           <input value={password} onChange={(e)=>setPassword(e.target.value)} type="password" aria-placeholder='Password'/>
           <small>This is error placeholder</small>
           <button>Login</button>
