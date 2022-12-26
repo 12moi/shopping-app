4b45a3beae393d16b4f2333c15c73ee859c23cd3
devmoi <moishadrack.com>
2022-12-26 09:19:19 +0300

    add login state

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,12 +1,20 @@
-import React from 'react'
+import React, {useState} from 'react'
 import '../App.css'
 
-const login = () => {
+const Login = () => {
+  const {userName, setUsername}=useState("");
+  const {password, setPassword}=useState("");
+  const {error, stError}=useState("");
   return (
     <div className='login'>
-        login
+        <div>
+          <input value={userName} onChange={(e)=>setUsername(e.target.value)} type="text" placeholder='Username'/>
+          <input value={password} onChange={(e)=>setPassword(e.target.value)} type="password" aria-placeholder='Password'/>
+          <small>This is error placeholder</small>
+          <button>Login</button>
         </div>
+    </div>
   )
 }
 
-export default login
+export default Login
